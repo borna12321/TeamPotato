c86ceee3dda56de962b7ff2fdb747f2cb210a38f
borna12321 <bormo45@gmail.com>
2020-09-17 14:02:29 +0200

    99% FINAL VERSION

--- FarmlandGameV1/Assets/Scripts/BuildingDatabase.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BuildingDatabase : MonoBehaviour
-{
-   public List<Building> buildingDatabase = new List<Building>();
-}
